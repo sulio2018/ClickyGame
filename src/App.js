bb713383f2b5015557acf8ae84ed8d51a63cd9f3
jmartinez <jcmartinez1925@gmail.com>
2019-10-25 10:36:43 -0700

    establish components and debug

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Images from "./components/Images";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // Set variables as states
+  state = {
+    score: 0,
+    topScore: 0,
+    id: [],
+    message: "Start Clicking!"
+  }
+
+  // Function for clicking image
+  handleImageClick = event => {
+    const imgId = event.target.id;
+
+    // If click same image, reset score. Else, save img to array 
+    if (this.state.id.includes(imgId)) {
+      this.setState({
+        score: 0,
+        id: [],
+        message: "You already chose that team."
+      })
+    }
+    else {
+      this.state.id.push(imgId);
+
+      // Update score
+      if (this.state.topScore <= this.state.score) {
+        this.setState({
+          topScore: this.state.score + 1,
+          score: this.state.score + 1
+        })
+      }
+      else {
+        this.setState({
+          score: this.state.score + 1
+        })
+      }
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <Navbar message={this.state.message} score={this.state.score} topScore={this.state.topScore}/>
+        <Header />
+        <Images handleImageClick={this.handleImageClick}/>
+        <Footer />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.css";
+
+function Footer() {
+    return (
+        <nav class="navbar justify-content-center navbar-dark bg-dark" id="footer">
+            <div class="footer">Clicky Game! Created with React.</div>
+        </nav>
+    );
+}
+
+export default Footer;
